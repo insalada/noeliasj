e8e7c40131ab4b7a46ff4ce09bc6e4a1219a8d69
insalada <insalada@gmail.com>
2015-10-03 23:53:26 +0200

    several changes on main page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('#noemain').fadeIn(1000);
+    $('#main').delay(500).fadeIn(1000);
+
+    $('#maintitle').mouseenter(function() {
+    });
+    
+    $('#maintitle').mouseleave(function() {
+    });
+
+});

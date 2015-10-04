ea7a0b54b720c3942a5275b79cc0c18b3721604e
insalada <insalada@gmail.com>
2015-10-04 21:22:53 +0200

    new styles and icons

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
 
     $('#noemain').fadeIn(1000);
     $('#main').delay(500).fadeIn(1000);
+    $('#areas').delay(1000).fadeIn(1000);
 
-    $('#maintitle').mouseenter(function() {
-    });
-    
-    $('#maintitle').mouseleave(function() {
-    });
 
+    $('.bs-icons li').mouseenter(function() {
+        $(this).fadeTo("fast", 1);
+    });
+    $('.bs-icons li').mouseleave(function() {
+        $(this).fadeTo("slow", 0.2);
+    });
 });
